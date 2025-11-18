28c748bfbc5af60deb02de31cc7365553984cf26
Vitor Carvalho <vcml@pm.me>
2025-11-17 22:00:56 -0300

    updating

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import os
 
-from flask import Flask
-from flask import url_for
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    version = os.getenv('VERSION')
-    hostname = os.getenv('HOSTNAME')
-    response = {
-        "app": f"hello-{version}",
-        "hostname": f"{hostname}"
-    }
+    version = os.getenv("VERSION")
+    hostname = os.getenv("HOSTNAME")
+    response = {"app": f"flask-app-{version}", "hostname": f"{hostname}"}
     return response
+
 
 @app.route("/about")
 def about():
-    response = {
-        message: "this should be about the app"
-    }
-    return message
+    response = {"message": "this should be about the app"}
+    return response
